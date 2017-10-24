293c52a8fb91b73d805a9d4a527ac221aff92b14
Vincent <Vincent@Vincent-PC>
2017-10-24 08:14:29 -0700

    Minor correction to graphing logic. Added graph func.

--- main.js
+++ main.js
@@ -228,10 +228,10 @@ Automata.prototype.draw = function (ctx) {
 		ctx.strokeStyle = "black";
 		ctx.rect(start_x + (row * size), start_y + (col * size), size, size);
 		ctx.stroke();
-		col++;
-		if (col > 9) {
-			row++;
-			col = 0;
+		row++;
+		if (row > (graph_width / size)) {
+			col++;
+			row = 0;
 		}
 	}
 	
@@ -254,6 +254,29 @@ function setParameters() {
 	gameEngine.entities = [];
 	gameEngine.addEntity(automata);
 
+}
+
+function graph(ctx, arr, max, count, x, y, width, height, style, text) {
+    if(text && arr.length > 0) {
+        ctx.fillStyle = "Black";
+        var current = parseFloat(arr[arr.length - 1].toFixed(3));
+        ctx.fillText(text + ": " + current + " Max: " + parseFloat(max.toFixed(3)), x, y + 10);
+    }
+
+    ctx.strokeStyle = style;
+    var px = 0;
+    var step = width / count;
+    var range = max/height;
+    var startY = y + height;
+
+    var i = Math.max(0, arr.length - count); //display the last (max) events
+    ctx.moveTo(x, startY - arr[i]/height);
+    ctx.beginPath();
+    while(i < arr.length) {
+        ctx.lineTo(x + px++ * step, startY - arr[i]/range);
+        i++;
+    }
+    ctx.stroke();
 }
 //the "main" code begins here
 
